4430fd8848a8422e29b565006de4cc6665e1114e
nitinmakwanaahir <nitinmakwanaahir@gmail.com>
2026-01-22 08:11:11 +0530

    Add files via upload

---
+++ ASSIGMENT-1 NITIN.PYTH.PY
@@ -0,0 +1,83 @@
+#que -1
+
+a= "welcome"
+print (a)
+
+#que-2
+
+b=("rajesh kumar",
+   "\t flat no.101,sunshine aparment",
+   "\t MG ROAD,sectore 15",
+   "\t rajkot",
+   "\t pincode:360004",
+   "/t india")
+print(b)
+
+#que-3
+
+a=150
+b=120.50
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#que-4
+
+radius=float(input("enter radius"))
+
+area=3.14*radius*radius
+cercalframs=2*3.14*radius
+
+print(area)
+print(cercalframs)
+
+#que-5
+
+p=float(input("enter perimal amount :"))
+r=float(input("enter rate of intrest :"))
+t=float(input("enter time in years :"))
+
+si=(p*r*t)/100
+print(si)
+
+#que-6
+
+length=float(input("emter length :"))
+width=float(input("enter width"))
+
+perimeter=2*(length+width)
+print(perimeter)
+
+#que-7
+
+length=float(input(enter leanth:))
+width=float(input(emter width :))
+
+area=length*width
+perimeter=2*(length+width)
+
+print(area)
+print(perimeter)
+
+#que-8
+a=float(input("enter side a:"))
+b=float(input("enter side b:"))
+c=float(input("enter side c:"))d
+perimeter=a+b+c
+print(perimeter)
+
+#que-9
+
+area=side*side
+perimeter=4*side
+
+print(area)
+print(perimeter)
+
+#que-10
+
+side=float(input("enter side of area"))
+
+perimeter=4*side
+print(perimeter)
