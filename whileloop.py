a08b6e36487591304efc1f9c99607ce94de29611
nitinmakwanaahir <nitinmakwanaahir@gmail.com>
2026-02-23 11:38:41 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,27 @@
+"""i = 1
+
+while i <= 5:
+    print(i)"""
+    
+#2 Print Even Numbers from 2 to 10
+"""num = 2
+
+while num <= 10:
+    print(num)
+    num += 2"""
+    
+"""#3 Sum of First 5 Natural Numbers
+num = int(input("enter value:"))
+i = 1
+sum = 0
+while i <= num:
+    sum = sum + i
+    i = i + 1
+    print("your sum is :", sum)"""
+    
+#4 Multiplication Table 
+num = 1
+
+while num <= 10:
+    print("3 x", num, "=", 3 * num)
+    num += 1
